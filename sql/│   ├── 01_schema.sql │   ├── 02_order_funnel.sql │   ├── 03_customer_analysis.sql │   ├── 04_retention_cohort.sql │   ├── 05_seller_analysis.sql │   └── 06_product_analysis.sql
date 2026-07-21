7a76b05a876a70f3766a7bf92007e33ad2254a95
parisa <141882559+parisaadr@users.noreply.github.com>
2026-07-21 16:28:20 +0330

    Delete sql directory

--- sql/│   ├── 01_schema.sql │   ├── 02_order_funnel.sql │   ├── 03_customer_analysis.sql │   ├── 04_retention_cohort.sql │   ├── 05_seller_analysis.sql │   └── 06_product_analysis.sql
+++
@@ -1,7 +0,0 @@
-sql/
-│   ├── 01_schema.sql
-│   ├── 02_order_funnel.sql
-│   ├── 03_customer_analysis.sql
-│   ├── 04_retention_cohort.sql
-│   ├── 05_seller_analysis.sql
-│   └── 06_product_analysis.sql
